f119ca1972d59a654f52ba44fd7bde1aae0f2b81
Mr. Mario <gokulsujan.dev@gmail.com>
2025-08-16 01:33:08 +0530

    Load .env file in main

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"net/http"
+	"treeforms_billing/logger"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		logger.Danger("Error while loading .env file. Message: " + err.Error())
+		return
+	}
+
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
