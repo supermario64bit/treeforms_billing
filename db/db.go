62cea72ec7bd38b68adf3cb3461a88075003d332
Mr. Mario <gokulsujan.dev@gmail.com>
2025-08-16 01:43:25 +0530

    Database changes added

---
+++ db/db.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"os"
+	"treeforms_billing/logger"
+	"treeforms_billing/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Get() *gorm.DB {
+	if db != nil {
+		return db
+	}
+	var err error
+
+	dsn := os.Getenv("DB_DSN")
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logger.Danger("Database connection failed. Message: " + err.Error())
+		return nil
+	}
+	return db
+}
+
+func Automigrate() {
+	db.AutoMigrate(
+		models.User{},
+	)
+}
