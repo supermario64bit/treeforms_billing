a17afb709b595fbba85c58e4d00047172aaf962e
Mr. Mario <gokulsujan.dev@gmail.com>
2025-08-17 20:16:12 +0530

    Added loggers for Create and Find user controller.

--- controller/user.go
+++ controller/user.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 	"treeforms_billing/dtos"
+	"treeforms_billing/logger"
 	"treeforms_billing/models"
 	"treeforms_billing/services"
 
@@ -24,10 +25,12 @@ func NewUserController() UserController {
 }
 
 func (ctlr *userController) Create(c *gin.Context) {
+	logger.Info("API Request for creating a user.")
 	var userDTO *dtos.UserDTO
 	err := c.ShouldBindBodyWithJSON(userDTO)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": "Invalid Request Body", "result": gin.H{"error": err.Error()}})
+		logger.Info("Create user api stopped due to request body is invalid")
 		return
 	}
 
@@ -35,16 +38,21 @@ func (ctlr *userController) Create(c *gin.Context) {
 		Role: userDTO.Role, Status: userDTO.Status})
 	if appErr != nil {
 		appErr.WriteHTTPResponse(c)
+		logger.Info("Create user api stopped")
 		return
 	}
 
 	c.JSON(http.StatusBadRequest, gin.H{"status": "success", "message": "User Created", "result": gin.H{"user": user}})
+	logger.Info("Create user api finished")
+
 }
 
 func (ctrl *userController) Find(c *gin.Context) {
+	logger.Info("API Request for finding users.")
 	filter := &models.UserFilter{}
 	err := c.ShouldBindBodyWithJSON(filter)
 	if err != nil {
+		logger.Info("Find user api stopped due to request body is invalid")
 		c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": "Invalid Request Body", "result": gin.H{"error": err.Error()}})
 		return
 	}
@@ -52,9 +60,10 @@ func (ctrl *userController) Find(c *gin.Context) {
 	users, appErr := ctrl.svc.Find(*filter)
 	if appErr != nil {
 		appErr.WriteHTTPResponse(c)
+		logger.Info("Find users api stopped")
 		return
 	}
 
 	c.JSON(http.StatusBadRequest, gin.H{"status": "success", "message": "User Created", "result": gin.H{"users": users}})
-
+	logger.Info("Find users api finished")
 }
